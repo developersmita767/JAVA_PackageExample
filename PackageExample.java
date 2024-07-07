e86d14b308814aed7f7f28a9f0e82dea6e57a571
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-07-08 00:26:52 +0530

    Add files via upload

---
+++ PackageExample.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class PackageExample{
+    public void m1(String message){
+        System.out.println(message);
+    }
+    public static void main(String ar[]){
+        System.out.println("Hello world");
+    }
+}
